6cba0db7ec893047a44a4f74adc5d65efcc9b6c5
ramang22 <wibrach@gmail.com>
2021-10-23 14:56:21 +0200

    partial import done

---
+++ python_import/import.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from psycopg2.extras import RealDictCursor
+import psycopg2
+
+df = pd.read_csv("part_data.csv")
+conn = psycopg2.connect("dbname=climathon user=postgres password=root host=localhost")
+cur = conn.cursor()
+
+rows = zip(df.hash,df.Objem, df['Materiál nádoby'],df['KU - městská část'],df['KU - městský obvod'],
+           df['KU - ulice'],df['lat'],df['long'],df['KU - číslo orientační'],df['Název stanoviště'],df['Číslo stanoviště'],df['Typ odpadu'],
+           df['Kód nádoby'],df['Počet nádob'],df['Rajón'],df['T1'],df['T2'],df['T3'],df['T4'],df['Provozovna'])
+
+
+
+
+cur.executemany("""
+ INSERT INTO bins(id, volume_in_litres, material, city_part, city_district, street, lat, lng, orientation_number, station_name, station_number, waste_type, bin_code, bin_count, region, t1, t2, t3, t4, establishment)
+ (%s, %s,%s,%s,%s""", rows)
+
+
+conn.commit()
+cur.close()
+conn.close()
